84c56cf23ec808ba2de23bb2a2489cc107811ab1
Gabriel Racz <gabriel.s.racz@gmail.com>
2023-03-10 22:42:54 -0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use image::{Rgba, ImageBuffer};
+#![allow(dead_code)]
+
+use image::{Rgba, ImageBuffer, imageops};
 use std::{env, process::exit};
 use std::path::Path;
-use std::ffi::OsStr;
+// use std::ffi::OsStr;
 
 #[non_exhaustive]
 struct Colors;
@@ -46,7 +48,8 @@ impl Bayer {
 }
 
 
-fn ordered_dither(image: &ImageBuffer<Rgba<u8>, Vec<u8>>, level: i32) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+
+fn ordered_dither(image: &ImageBuffer<Rgba<u8>, Vec<u8>>, level: i32, darkness: f32) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
     let kernel: &[&[f32]];
     match level {
         1 => {kernel = Bayer::_L1},
@@ -54,12 +57,13 @@ fn ordered_dither(image: &ImageBuffer<Rgba<u8>, Vec<u8>>, level: i32) -> ImageBu
         3 => {kernel = Bayer::_L3},
         _ => {kernel = Bayer::_L2}
     }
+
     let mut result: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::new(image.width(), image.height());
     for x in 0..image.width() {
         for y in 0..image.height() {
             let p = image.get_pixel(x, y);
             let intensity: f32 = (p[0] as u32 + p[1] as u32 + p[2] as u32) as f32/(3.0f32*255.0f32);
-            let threshhold = 1.03f32 - kernel[x as usize%kernel.len()][y as usize %kernel.len()];
+            let threshhold = kernel[x as usize%kernel.len()][y as usize %kernel.len()] + darkness;
 
             let dithered: Rgba<u8>;
             if intensity > threshhold {
@@ -76,27 +80,77 @@ fn ordered_dither(image: &ImageBuffer<Rgba<u8>, Vec<u8>>, level: i32) -> ImageBu
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut bayer_level: i32 = 2;
-    let path_string: &str;
-    let usage_string = "usage: dither [image] [detail level] [darkness]";
+    let mut darkness: f32 = 0.0;
+    let input_path_string: &str;
+    let output_path_string: &str;
+    let usage_string = "usage: dither [input] [output] [detail level] [darkness]";
     
-    if args.len() > 1{
-        path_string = &args[1];
+    if args.len() > 2{
+        input_path_string = &args[1];
+        output_path_string = &args[2];
     } else {
         println!("{}", usage_string);
         exit(0);
     }
-    if args.len() > 2 {
-        bayer_level = args[2].parse::<i32>().expect(usage_string);
+    if args.len() > 3 {
+        bayer_level = args[3].parse::<i32>().expect("invalid detail level");
+    }
+    if args.len() > 4 {
+        darkness = args[4].parse::<f32>().expect("invalid darkness value");
     }
 
-    let path = Path::new(path_string);
+    let path = Path::new(input_path_string);
     let file_stem = path.file_stem().unwrap();
     let extension = path.extension().unwrap();
 
-    let source_image = image::open(path_string).expect("Failed to open input image").into_rgba8();
-    let dithered_image = ordered_dither(&source_image, bayer_level);
+    let original = image::open(input_path_string).expect("Failed to open input image").into_rgba8();
+    // let resampled = imageops::resize(&original, original.width()/2, original.height()/2, imageops::FilterType::Nearest);
+    let resampled = &original;
+    let dithered_image = ordered_dither(&resampled, bayer_level, darkness);
+    let output = imageops::resize(&dithered_image, original.width(), original.height(), imageops::FilterType::Nearest);
 
     let dithered_filename: String = file_stem.to_str().unwrap().to_owned() + "-dithered." + extension.to_str().unwrap();
-    dithered_image.save(&dithered_filename).expect("Failed to save image");
+    output.save(output_path_string).expect("Failed to save image");
     println!("Saved dithered image to: {}", dithered_filename);
+}
+
+// https://www.includehelp.com/rust/reverse-bits-of-a-binary-number.aspx
+fn bit_reverse(n: u8) -> u8 {
+    let mut val: u8 = 0;
+    let mut tmp: u8;
+    let mut rev: u8 = 0;
+
+    while val < 8 {
+        tmp = n & (1 << val);
+        if tmp>0
+        {
+            rev = rev | (1 << ((8 - 1) - val));
+        }
+        val = val + 1;
+    }
+    return rev;
+}
+
+// https://graphics.stanford.edu/~seander/bithacks.html#InterleaveBMN
+fn bit_interleave(i: u32, j: u32) -> u32 {
+    const B: [u32; 4] = [0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF];
+    const S: [u32; 4] = [1, 2, 4, 8];
+
+    let mut x: u32 = i;
+    let mut y: u32 = j;
+    let z: u32;
+
+    x = (x | (x << S[3])) & B[3];
+    x = (x | (x << S[2])) & B[2];
+    x = (x | (x << S[1])) & B[1];
+    x = (x | (x << S[0])) & B[0];
+
+    y = (y | (y << S[3])) & B[3];
+    y = (y | (y << S[2])) & B[2];
+    y = (y | (y << S[1])) & B[1];
+    y = (y | (y << S[0])) & B[0];
+
+    z = x | (y << 1);
+
+    return z;
 }
